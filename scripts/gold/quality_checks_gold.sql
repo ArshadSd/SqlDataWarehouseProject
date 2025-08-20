a4a3a1c32bb0d274174bcb5d4eb6d032fd7e9afa
ArshadSyed <127642794+ArshadSd@users.noreply.github.com>
2025-08-20 15:53:59 +0530

    Update quality_checks_gold.sql

--- scripts/gold/quality_checks_gold.sql
+++ scripts/gold/quality_checks_gold.sql
@@ -1,1 +1,68 @@
+SELECT cst_id , COUNT(*) FROM (
+SELECT 
+	ci.cst_id,
+	ci.cst_key,
+	ci.cst_firstname,
+	ci.cst_lastname,
+	ci.cst_material_status,
+	ci.cst_gndr,
+	ci.cst_create_date,
+	ca.bdate,
+	ca.gen,
+	la.cntry
+
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca
+ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+ON ci.cst_key = la.cid
+)t GROUP BY cst_id
+HAVING COUNT (*)> 1 
+
+---------------------------------------
+SELECT DISTINCT
+	ci.cst_gndr,
+	ca.gen
+
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca
+ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+ON ci.cst_key = la.cid
+ORDER BY 1,2
+
+----------------------------
+SELECT DISTINCT new_gen FROM gold.dim_customers
+--------------------------------
+SELECT prd_key ,COUNT(*)  FROM (
+SELECT 
+	pn.prd_id,
+	pn.cat_id,
+	pn.prd_key,
+	pn.prd_nm,
+	pn.prd_cost,
+	pn.prd_line,
+	pn.prd_start_dt,
+	pc.cat,
+	pc.subcat,
+	pc.maintenance
+
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+ON pn.cat_id = pc.id
+WHERE prd_end_dt IS NULL
+)t GROUP BY prd_key
+HAVING COUNT(*) > 1
+
+----------------------------
+
+
+
+
+
+
+
+
+
+
 
